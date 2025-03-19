2ea0307455f2da077900dae3a5559359f13ea7db
PriyomDatta <priyomdat@gmail.com>
2025-03-19 15:41:46 +0000

    Adding my first configuration file

--- Pro_002/src/main.cpp
+++ Pro_002/src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "../build/Versionconfig.h"
 #include"../inc/Mod_1.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Project major version is :- " << PROJECT_VERSION_MAJOR<<" .";
+    std::cout << "\nProject minor version is :- " << PROJECT_VERSION_MINOR<<" .";
+    std::cout << "\nHello, World!" << std::endl;
     fun1(1);
-    std::cout << "\nFirst function called";
+    std::cout << "\nFirst function called\n";
     return 0;
 }
